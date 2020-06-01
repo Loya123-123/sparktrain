fddcfd0327e529fce2ac0e0fcf484ed4d8bbc5a3
exiaozhong <exiaozhong@163.com>
2020-06-01 10:24:46 +0800

    flink 窗口 水位

--- FlinkTest/src/main/scala/com/yjz/apitest/WindowTest.scala
+++ FlinkTest/src/main/scala/com/yjz/apitest/WindowTest.scala
@@ -1,12 +1,14 @@
 package com.yjz.apitest
 
+import org.apache.flink.api.common.state.{ValueState, ValueStateDescriptor}
 import org.apache.flink.streaming.api.TimeCharacteristic
-import org.apache.flink.streaming.api.functions.AssignerWithPeriodicWatermarks
 import org.apache.flink.streaming.api.functions.timestamps.BoundedOutOfOrdernessTimestampExtractor
+import org.apache.flink.streaming.api.functions.{AssignerWithPeriodicWatermarks, KeyedProcessFunction}
 import org.apache.flink.streaming.api.scala._
 import org.apache.flink.streaming.api.watermark.Watermark
 import org.apache.flink.streaming.api.windowing.assigners.SlidingEventTimeWindows
 import org.apache.flink.streaming.api.windowing.time.Time
+import org.apache.flink.util.Collector
 
 
 object WindowTest {
@@ -14,7 +16,7 @@ object WindowTest {
   def main(args: Array[String]): Unit = {
     val env = StreamExecutionEnvironment.getExecutionEnvironment
     env.setParallelism(1)
-    env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime)
+    env.setStreamTimeCharacteristic(TimeCharacteristic.ProcessingTime)
     // 周期水位时间 ，默认是200
     env.getConfig.setAutoWatermarkInterval(100L)
 
@@ -23,7 +25,6 @@ object WindowTest {
     val host = "localhost"
     val port = 7777
     val stream = env.socketTextStream(host,port)
-
 
     val dataStream = stream.map(data => {
       val dataArray = data.split(",")
@@ -37,7 +38,7 @@ object WindowTest {
       // 自定义水位
       // 时间线 左闭右开
       .assignTimestampsAndWatermarks(new BoundedOutOfOrdernessTimestampExtractor[SensorReading](Time.seconds(1)) {
-      override def extractTimestamp(t: SensorReading): Long = t.timestamp * 1000L
+      override def extractTimestamp(t: SensorReading): Long = t.timestamp * 10000L
     })
       .map(
       data => (data.id,data.temperature))
@@ -48,14 +49,64 @@ object WindowTest {
 
       .reduce((x,y)=>(x._1,x._2.min(y._2)))
 
+
     dataStream.print("input data")
     minTemperwindowStream.print("output data")
+
+
+
+    dataStream.keyBy(_.id).process(new MyProcess()).print()
+
 
     env.execute("window job")
 
   }
 }
 
+class MyProcess() extends KeyedProcessFunction[String,SensorReading,String] {
+
+  //定义一个状态，用来保存上一个数据的温度值 上下文获取 定义状态
+  lazy val lastTemp : ValueState[Double] = getRuntimeContext.getState(new ValueStateDescriptor[Double]("lastTemp",classOf[Double]))
+
+  // 定义一个状态 ， 用来保存定时器的时间戳
+
+   lazy val currentTimer : ValueState[Long] = getRuntimeContext.getState(new ValueStateDescriptor[Long]("currentTimer",classOf[Long]))
+
+  override def processElement(value: SensorReading, ctx: KeyedProcessFunction[String, SensorReading, String]#Context, out: Collector[String]): Unit = {
+    // 先取出上一个温度值
+    val preTemp = lastTemp.value()
+    // 更新温度值
+    lastTemp.update(value.timestamp)
+
+    val curTimerTs = currentTimer.value()
+
+
+    // 温度上升
+    if (value.timestamp > preTemp && curTimerTs == 0){
+      // 当前时间
+      val timerTs = ctx.timerService().currentProcessingTime() + 1000L
+      // 定义 定时器
+      ctx.timerService().registerProcessingTimeTimer(timerTs)
+      // 当前时间值存储
+      currentTimer.update(timerTs)
+    }else if (preTemp > value.timestamp || preTemp == 0.0){
+      // 如果温度下降 或 是第一条数据，删除定时器，并清空状态
+        ctx.timerService().deleteProcessingTimeTimer(curTimerTs)
+        currentTimer.clear()
+    }
+    ctx.getCurrentKey
+  }
+
+  override def onTimer(timestamp: Long, ctx: KeyedProcessFunction[String, SensorReading, String]#OnTimerContext, out: Collector[String]): Unit  = {
+  // 直接输出报警信息
+    out.collect(ctx.getCurrentKey + "温度上升 ")
+    currentTimer.clear()
+  }
+}
+
+/**
+  * 自定义水位线以及处理时间字段
+  */
 class MyAssigner extends AssignerWithPeriodicWatermarks[SensorReading]{
   // 延迟
   val bound = 6000
@@ -71,3 +122,9 @@ class MyAssigner extends AssignerWithPeriodicWatermarks[SensorReading]{
     t.timestamp * 1000
   }
 }
+
+
+
+
+
+
